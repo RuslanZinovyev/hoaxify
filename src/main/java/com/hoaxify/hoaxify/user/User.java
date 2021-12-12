747e890c84d51567d099715be2170ef7b9f9679e
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-12-12 00:23:19 -0500

    removed unused imports

--- src/main/java/com/hoaxify/hoaxify/user/User.java
+++ src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
